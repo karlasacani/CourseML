2d8819e3db08dd605d0944dce2fff67fd2fc8393
karlasacani <karlahorranas@gmail.com>
2022-08-10 19:14:59 -0300

    data prepared

--- code/dataload.r
+++ code/dataload.r
@@ -1,6 +1,26 @@
-Db <- read.csv("./dataraw/benthos_biomass.csv")
-summary(Db)
-str(Db)
+rm(list = ls()) 
+Db <- read.csv("./dataraw/rca_data_2012_2022-06-29.csv")
 
+Db$CellIDFac <- as.factor(Db$CellID)
+str(Db$CellIDFac)
 
-sdds
+require(tidyverse)
+Db <- Db %>% select(CellIDFac, Date, SAL_avg)
+
+aux1 <- list()
+
+aux1[[1]] <- Db[Db$CellIDFac==levels(Db$CellIDFac)[1],c(1)]
+for (i in 1:length(levels(Db$CellIDFac))){
+    aux1[[i]] <- Db[Db$CellIDFac==levels(Db$CellIDFac)[i],c(3)]
+}
+
+aux2<-do.call(cbind.data.frame,aux1)
+colnames(aux2)<-levels(Db$CellIDFac)
+
+aux3 <- seq.Date(from = as.Date("2012-01-01"), to = as.Date("2013-01-01"), by = "1 day")
+require(lubridate)
+aux4 <- data.frame(Year = year(aux3), Month = month(aux3), Day = day(aux3))
+
+data_prepared <- cbind(aux4,aux2)
+
+saveRDS(data_prepared, file = "data_prepared.rds")
